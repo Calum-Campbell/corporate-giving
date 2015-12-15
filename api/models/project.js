a3d1a634666a07d25a9f7d367a55bda143d71e74
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-12-15 11:35:28 +0000

    Scraper working

--- api/models/project.js
+++ api/models/project.js
@@ -1,11 +1,32 @@
 var mongoose = require("mongoose");
 
-var projectSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  need: { type: String, required: true },
-  themeName: { type: String, required: true },
-  summary: { type: String, required: true },
-  imageLink: { type: String, required: true },
-});
+var projectSchema = mongoose.Schema({ 
+          projectId: {type: Number},
+          organization: {type: String},
+          title: {type: String},
+          summary: {type: String},
+          contactCity: {type: String},
+          contactCountry: {type: String},
+          contactPhone: {type: String},
+          contactUrl: {type: String},
+          projectLink: {type: String},
+          progressReportLink: {type: String},
+          themeName: {type: String},
+          country: {type: String},
+          iso3166CountryCode: {type: String},
+          region: {type: String},
+          goal: {type: Number},
+          funding: {type: Number},
+          remaining: {type: Number},
+          numberOfDonations: {type: Number},
+          need: {type: String},
+          longTermImpact: {type: String},
+          activities: {type: String},
+          imageLink: {type: String},
+          longitude: {type: Number},
+          latitude: {type: Number}
+          })
+      
+
 
 module.exports = mongoose.model('Project', projectSchema);
