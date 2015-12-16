ad783f34f1b9853abb38bb7356b96ea1005cf7e7
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-12-16 16:05:25 +0000

    images added

---
+++ api/db/seeds.js
@@ -0,0 +1,104 @@
+[
+    {
+        "_id": "567002e91e189900786b3457",
+        "id": "animals",
+        "name": "Animals",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3458",
+        "id": "children",
+        "name": "Children",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3459",
+        "id": "climate",
+        "name": "Climate Change",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b345a",
+        "id": "democ",
+        "name": "Democracy and Governance",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b345b",
+        "id": "disaster",
+        "name": "Disaster Recovery",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b345c",
+        "id": "ecdev",
+        "name": "Economic Development",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b345d",
+        "id": "edu",
+        "name": "Education",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b345e",
+        "id": "env",
+        "name": "Environment",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b345f",
+        "id": "finance",
+        "name": "Microfinance",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3460",
+        "id": "gender",
+        "name": "Women and Girls",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3461",
+        "id": "health",
+        "name": "Health",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3462",
+        "id": "human",
+        "name": "Humanitarian Assistance",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3463",
+        "id": "rights",
+        "name": "Human Rights",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3464",
+        "id": "sport",
+        "name": "Sport",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3465",
+        "id": "tech",
+        "name": "Technology",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3466",
+        "id": "hunger",
+        "name": "Hunger",
+        "__v": 0
+    },
+    {
+        "_id": "567002e91e189900786b3467",
+        "id": "art",
+        "name": "Arts and Culture",
+        "__v": 0
+    }
+]
