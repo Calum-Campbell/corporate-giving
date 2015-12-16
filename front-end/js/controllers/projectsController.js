7c415143a32499e0b29b1e46340872a72194076f
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-12-16 10:12:49 +0000

    Working add remove themes buttons

--- front-end/js/controllers/projectsController.js
+++ front-end/js/controllers/projectsController.js
@@ -32,6 +32,7 @@ self.getUser = function(){
     self.user = data.user
     self.userProjects = self.user.projects;
     self.userThemes = self.user.themes;
+    console.log(data.user)
   })
 };
 
--- front-end/js/controllers/themesController.js
+++ front-end/js/controllers/themesController.js
@@ -14,33 +14,6 @@ function ThemesController(Theme, $http, User, CurrentUser, TokenService){
   self.userThemes = [];
 
 
-  self.getWebThemeNames = function(){
-    $http({
-      method: 'GET',
-      url: 'https://api.globalgiving.org/api/public/projectservice/themes?api_key=a310a8b0-2e3a-4c23-aedf-ec13bf0e00a3',
-      headers: {
-        'Accept': 'application/json'
-      }
-    }).then(function(response){
-      var themes = response.data.themes
-      console.log(themes.theme.length)
-      for (var i = themes.theme.length - 1; i >= 0; i--) {
-        self.addWebTheme(themes.theme[i])
-      };
-    })
-  }
-
-  self.addWebTheme = function(themeData){
-    var newThemeObject = {
-      name: themeData['name'],
-    }
-    console.log(newThemeObject)
-    var theme = { theme: newThemeObject }
-    Theme.save(theme, function(data){
-     self.all.push(data);
-     self.theme = {};
-   })
-  }
 
   self.getUsers = function(){
    User.query(function(data){
@@ -49,6 +22,7 @@ function ThemesController(Theme, $http, User, CurrentUser, TokenService){
   }
 
   self.getThemes = function(){
+
     Theme.query(function(data){
       return self.all = data;
     })
@@ -64,16 +38,22 @@ function ThemesController(Theme, $http, User, CurrentUser, TokenService){
   };
 
   self.checkTheme = function(themeId){
-    return self.userThemes.indexOf(themeId) === -1 ? false : true;
-  }
+    // self.getUser();
+    console.log(self.userThemes)
+    for (var i = 0; i < self.userThemes.length; i++) {
+
+      if(self.userThemes[i]._id === themeId) return true
+    };
+    return false
+  };
 
   self.addThemeToUser = function(theme){
-    self.user = TokenService.decodeToken();
+    // self.user = TokenService.decodeToken();
     var data = {
       themeId: theme._id
     }
     User.addTheme({id: self.user._id}, data, function(user){
-      self.userThemes.push(theme._id);
+      self.userThemes.push(theme);
       self.checkTheme(theme._id);
     });
   }
@@ -89,8 +69,8 @@ function ThemesController(Theme, $http, User, CurrentUser, TokenService){
     });
   }
 
-  self.getThemes();
   self.getUsers();
   self.getUser();
+  self.getThemes();
 
 }
